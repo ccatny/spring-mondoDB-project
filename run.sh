6000e0dda884ea3cec1576bb3368f5714e48900b
chengk <cheng12952@gmail.com>
2023-11-19 01:53:38 +0100

    reset dependency

--- run.sh
+++ run.sh
@@ -1,2 +1,14 @@
-mvn clean package
-mvn exec:java
+PID=$(ps -ef |grep springboot-mongo-demo.jar |grep -v grep |awk '{ print $2 }')
+echo PID $PID
+if [ -z "$PID" ]
+then
+    echo Application is already stopped
+else
+    echo kill $PID
+    kill $PID
+    echo stop springboot-mongo-demo
+fi
+
+nohup java -jar springboot-mongo-demo.jar   > nohup.out 2>&1 &
+
+echo start springboot-mongo-demo...
